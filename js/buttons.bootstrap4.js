dee9c2a0ce7195dc94f5623e6d2f896d323773e3
Allan Jardine <allan.jardine@sprymedia.co.uk>
2019-07-12 10:30:13 +0100

    added class flex-wrap to button container
    Sync to source repo @8d05a1c7a2ddd879d501a66d1a36a32e2ff586c9

--- js/buttons.bootstrap4.js
+++ js/buttons.bootstrap4.js
@@ -38,7 +38,7 @@ var DataTable = $.fn.dataTable;
 $.extend( true, DataTable.Buttons.defaults, {
 	dom: {
 		container: {
-			className: 'dt-buttons btn-group'
+			className: 'dt-buttons btn-group flex-wrap'
 		},
 		button: {
 			className: 'btn btn-secondary'
@@ -48,21 +48,13 @@ $.extend( true, DataTable.Buttons.defaults, {
 			className: 'dt-button-collection dropdown-menu',
 			button: {
 				tag: 'a',
-				className: 'dt-button dropdown-item',
-				active: 'active',
-				disabled: 'disabled'
+				className: 'dt-button dropdown-item'
 			}
 		}
-	},
-	buttonCreated: function ( config, button ) {
-		return config.buttons ?
-			$('<div class="btn-group"/>').append(button) :
-			button;
 	}
 } );
 
 DataTable.ext.buttons.collection.className += ' dropdown-toggle';
-DataTable.ext.buttons.collection.rightAlignClassName = 'dropdown-menu-right';
 
 return DataTable.Buttons;
 }));
